cacb9c44f273c1b3c7342f9a836ea2de586e9e02
Paul Bacchus <paulbacchus@P2X-3YZ.local>
2015-03-27 16:44:02 +0000

    one function answers.

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,43 +1,67 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce (fn [a b] (str a " " b)) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (rest (reduce (fn [acc elem] (conj (conj acc x) elem)) [] a-seq)))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [acc elem]
+                  (if (nil? elem)
+                    acc
+                    (inc acc)))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (vector (reduce min a-seq) (reduce max a-seq)))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (if (empty? sorted-seq)
+    (seq [n])
+    (if (> n (first sorted-seq))
+      (cons (first sorted-seq)
+            (insert (rest sorted-seq) n))
+      (cons n sorted-seq))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert '() a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce
+    (fn [a-set elem]
+      (if (contains? a-set elem)
+        (disj a-set elem)
+        (conj a-set elem)))
+    #{} a-seq))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& params]
+  (count params))
 
-(defn my-* [x]
-  :-)
+(defn my-* [& params]
+  (reduce * 1 params))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and [& params]
+  (fn [x]
+    ((fn [x preds]
+       (if (empty? preds)
+         true
+         (if ((first preds) x)
+           (recur x (rest preds))
+           false)))
+      x params)))
 
 (defn my-map [f a-seq]
-  [:-])
+  )
